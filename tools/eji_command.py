2ab28be0d6d6fc21e594f171b85c325b2329d1c9
Katherine Stevens Andersen <KAstev@gmail.com>
2023-02-08 09:56:33 -0700

    Update formatting

--- tools/eji_command.py
+++ tools/eji_command.py
@@ -11,34 +11,38 @@ from kauffman.tools import consistent_releases
 def _fetch_data_earnbeg_us(fetch_data):
     if fetch_data:
         df = qwi(['EarnBeg'], geo_level='us', private=True, annualize=True) \
-            [['time', 'EarnBeg']]. \
-            rename(columns={'EarnBeg': 'EarnBeg_us'})
+            [['time', 'EarnBeg']] \
+            .rename(columns={'EarnBeg': 'EarnBeg_us'})
     else:
-        df = pd.read_csv(c.filenamer(f'data/raw_data/earnbeg_us.csv')).astype({'time': 'int'})
+        df = pd.read_csv(c.filenamer(f'data/raw_data/earnbeg_us.csv')) \
+            .astype({'time': 'int'})
     joblib.dump(df, c.filenamer(f'data/temp/earnbeg_us.pkl'))
 
 
 def _fetch_data_qwi(region, fetch_data, qwi_n_threads):
     if fetch_data:
         print(f'\tcreating dataset neb/data/temp/qwi_{region}.pkl')
-        df = qwi(geo_level=region, firm_char=['firmage'], n_threads=qwi_n_threads)
+        df = qwi(
+            geo_level=region, firm_char=['firmage'], n_threads=qwi_n_threads
+        )
     else:
-        df = pd.read_csv(c.filenamer(f'data/raw_data/qwi_{region}.csv')). \
-            astype({'fips': 'str', 'time': 'int'})
+        df = pd.read_csv(c.filenamer(f'data/raw_data/qwi_{region}.csv')) \
+            .astype({'fips': 'str', 'time': 'int'})
     joblib.dump(df, c.filenamer(f'data/temp/qwi_{region}.pkl'))
 
 
 def _pep_county_adjustments(df, region):
     if region == 'county':
-        return df. \
-            assign(
+        return df \
+            .assign(
                 fips=lambda x: x.fips.replace(
                     {'02270':'02158', '46113':'46102', '51515':'51019'}
                 ),
-                region=lambda x: x.region.replace('Bedford city', 'Bedford County')
-            ). \
-            groupby(['time', 'fips', 'region', 'geo_level']).sum(). \
-            reset_index()
+                region=lambda x: x.region \
+                    .replace('Bedford city', 'Bedford County')
+            ) \
+            .groupby(['time', 'fips', 'region', 'geo_level']).sum() \
+            .reset_index()
     else:
         return df
 
@@ -46,22 +50,22 @@ def _pep_county_adjustments(df, region):
 def _fetch_data_pep(region, fetch_data):
     if fetch_data:
         print(f'\tcreating dataset neb/data/temp/pep_{region}.pkl')
-        df = pep(region). \
-            query('2001 <= time <= 2020'). \
-            pipe(_pep_county_adjustments, region). \
-            astype({'time': 'int', 'population': 'int'})
+        df = pep(region) \
+            .query('2001 <= time <= 2020') \
+            .pipe(_pep_county_adjustments, region) \
+            .astype({'time': 'int', 'population': 'int'})
     else:
-        df = pd.read_csv(c.filenamer(f'data/raw_data/pep_{region}.csv')). \
-            astype({'fips': 'str', 'time': 'int'})
+        df = pd.read_csv(c.filenamer(f'data/raw_data/pep_{region}.csv')) \
+            .astype({'fips': 'str', 'time': 'int'})
     joblib.dump(df, c.filenamer(f'data/temp/pep_{region}.pkl'))
 
 
 def _raw_data_fetch(fetch_data, qwi_n_threads):
     if fetch_data and not consistent_releases(n_threads=qwi_n_threads):
         raise Exception(
-            'There are multiple releases currently in use for the QWI data. ' \
+            'There are multiple releases currently in use for the QWI data. ' 
             'Please either wait for the Census to finish state updates on '
-            'the latest release, or assemble the data manually using the ' \
+            'the latest release, or assemble the data manually using the ' 
             'files at this link: https://lehd.ces.census.gov/data/qwi/'
         )
 
@@ -104,19 +108,15 @@ def _indicators_create(df):
 
     Parameters
     ----------
-    df : DataFrame
+    df: DataFrame
         The raw data
-
-    region : str
-        The geographical level of the data. Options: 'us', 'state', 'county', 'msa'
-
-    us_med : DataFrame
+    region: {'us', 'state', 'county', 'msa'}
+        The geographical level of data.
+    us_med: DataFrame
         Unconditional US-level earnings
-
-    start_year : int
+    start_year: int
         First year to be included in the final EJI csv
-
-    end_year : int
+    end_year: int
         Last year to be included in the final EJI csv
 
     Returns
@@ -126,24 +126,30 @@ def _indicators_create(df):
     """
     print('indicators_create...')
 
-    return df.\
-        assign(
-            # tee up values: I return a nan instead of the total if all age categories are not reported.
+    return df \
+        .assign(
+            # tee up values: I return a nan instead of the total if all age 
+            # categories are not reported.
             EmpMid=lambda x: (x['Emp'] + x['EmpEnd']) / 2,
-            total_emp=lambda x: x[['EmpMid', 'fips', 'time']].groupby(['fips', 'time']).transform(lambda y: y.sum() if y.count() == 5 else np.nan),
-        ).\
-        assign(
+            total_emp=lambda x: x[['EmpMid', 'fips', 'time']] \
+                .groupby(['fips', 'time']) \
+                .transform(lambda y: y.sum() if y.count() == 5 else np.nan),
+        ) \
+        .assign(
             # indicators create
             contribution=lambda x: x['EmpMid'] / x['total_emp'],
             compensation=lambda x: x['EarnBeg'] / x['EarnBeg_us'],
             constancy=lambda x: (x['EmpS'] / x['EmpTotal']),
             creation=lambda x: (x['EmpEnd'] - x['Emp'])/ x['population'] * 1000,
-        ). \
-        pipe(_missing_obs).\
-        query(f'{c.qwi_start_year} <= time <= {c.qwi_end_year}') \
-        [['fips', 'geo_level', 'firmage', 'time', 'contribution', 'compensation', 'constancy', 'creation']].\
-        sort_values(['fips', 'time', 'firmage']).\
-        reset_index(drop=True)
+        ) \
+        .pipe(_missing_obs) \
+        .query(f'{c.qwi_start_year} <= time <= {c.qwi_end_year}') \
+        [[
+            'fips', 'geo_level', 'firmage', 'time', 'contribution', 
+            'compensation', 'constancy', 'creation'
+        ]] \
+        .sort_values(['fips', 'time', 'firmage']) \
+        .reset_index(drop=True)
 
 
 def _fips_formatter(df, region):
@@ -152,11 +158,10 @@ def _fips_formatter(df, region):
 
     Parameters
     ----------
-    df : DataFrame
+    df: DataFrame
         Raw indicators data
-
-    region : str
-        The geographical level of data. Options: 'us', 'state', 'county', 'msa'
+    region: {'us', 'state', 'county', 'msa'}
+        The geographical level of data.
 
     Returns
     -------
@@ -166,9 +171,19 @@ def _fips_formatter(df, region):
     if region == 'us':
         return df.assign(fips='00')
     elif region == 'state':
-        return df.assign(fips=lambda x: x['fips'].apply(lambda row: row if len(row) == 2 else '0' + row))
+        return df.assign(
+            fips=lambda x: x['fips'] \
+                .apply(lambda row: row if len(row) == 2 else '0' + row)
+        )
     else:
-        return df.assign(fips=lambda x: x['fips'].apply(lambda row: '00' + row if len(row) == 3 else '0' + row if len(row) == 4 else row))
+        return df.assign(
+            fips=lambda x: x['fips'] \
+                .apply(
+                    lambda row: '00' + row if len(row) == 3 
+                    else '0' + row if len(row) == 4 
+                    else row
+                )
+        )
 
 
 def _final_jobs_formatter(df, region):
@@ -177,73 +192,91 @@ def _final_jobs_formatter(df, region):
 
     Parameters
     ----------
-    df : DataFrame
+    df: DataFrame
         The data
-
-    region : str
-        The geographical level of the data. Options: 'us', 'state', 'county', 'msa'
+    region: {'us', 'state', 'county', 'msa'}
+        The geographical level of data.
 
     Returns
     -------
     DataFrame
         The formatted indicators data
     """
-    return df. \
-        astype({'firmage': 'int'}).\
-        pipe(_fips_formatter, region).\
-        assign(
-            demographic=lambda x: pd.Categorical(x['firmage'].map(c.age_category_dict), ['Ages 0 to 1', 'Ages 2 to 3', 'Ages 4 to 5', 'Ages 6 to 10', 'Ages 11+']),
+    return df \
+        .astype({'firmage': 'int'}) \
+        .pipe(_fips_formatter, region) \
+        .assign(
+            demographic=lambda x: pd.Categorical(
+                x['firmage'].map(c.age_category_dict), 
+                [
+                    'Ages 0 to 1', 'Ages 2 to 3', 'Ages 4 to 5', 'Ages 6 to 10', 
+                    'Ages 11+'
+                ]
+            ),
             type='Age of Business'
-        ).\
-        rename(columns={'time': 'year', 'type':'demographic-type', 'firmage':'demographic-code'}).\
-        sort_values(['fips', 'year', 'demographic']). \
-        reset_index(drop=True)
+        ) \
+        .rename(columns={
+            'time': 'year', 'type':'demographic-type', 
+            'firmage':'demographic-code'
+        }) \
+        .sort_values(['fips', 'year', 'demographic']) \
+        .reset_index(drop=True)
 
 
 def _enforce_geo_universe(df, region):
     """
-    Enforce the presence of the entire universe of counties/msas/states for each year/firmage combo.
+    Enforce the presence of the entire universe of counties/msas/states for each
+    year/firmage combo.
 
     Parameters
     ----------
-    df : DataFrame
+    df: DataFrame
         The data
-
-    region : str
-        The geographical level of the data. Options: 'us', 'state', 'county', 'msa'
+    region: {'us', 'state', 'county', 'msa'}
+        The geographical level of data.
 
     Returns
     -------
     DataFrame
-        The complete data, with every fips code (within the input region) by year and firmage.
+        The complete data, with every fips code (within the input region) by 
+        year and firmage.
     """
     firmages = list(range(1,6))
 
-    return c.geography_universe[['fips', 'geo_level', 'name']].\
-        query(f'geo_level == "{region}"').\
-        drop_duplicates().\
-        assign(
+    return c.geography_universe[['fips', 'geo_level', 'name']] \
+        .query(f'geo_level == "{region}"') \
+        .drop_duplicates() \
+        .assign(
             year=lambda x: [[y for y in range(2001,2021)]]*len(x),
             type='Age of Business',
             code=lambda x: [firmages]*len(x)
-        ).\
-        explode('year').explode('code').\
-        assign(demographic=lambda x: x.code.map(c.age_category_dict)).\
-        rename(columns={'type':'demographic-type', 'code':'demographic-code'}).\
-        merge(df, on=['fips', 'geo_level', 'demographic-type', 'year', 'demographic', 'demographic-code'], how='left')
+        ) \
+        .explode('year').explode('code') \
+        .assign(demographic=lambda x: x.code.map(c.age_category_dict)) \
+        .rename(
+            columns={'type':'demographic-type', 'code':'demographic-code'}
+        ) \
+        .merge(
+            df, 
+            on=[
+                'fips', 'geo_level', 'demographic-type', 'year', 'demographic', 'demographic-code'
+            ], 
+            how='left'
+        )
 
 
 def final_data_transform(df, region):
     """
-    Perform some final cleanup on the indicators dataframe: Format the columns, add the region name as a column, and enforce the presence of the entire universe of counties/msas/states for each year/firmage combo.
+    Perform some final cleanup on the indicators dataframe: Format the columns,
+    add the region name as a column, and enforce the presence of the entire 
+    universe of counties/msas/states for each year/firmage combo.
 
     Parameters
     ----------
-    df : DataFrame
+    df: DataFrame
         The data
-
-    region : str
-        The geographical level of the data. Options: 'us', 'state', 'county', 'msa'
+    region: {'us', 'state', 'county', 'msa'}
+        The geographical level of data.
 
     Returns
     -------
@@ -251,9 +284,9 @@ def final_data_transform(df, region):
         The formatted data
     """
     print('final_data_transform')
-    return df.\
-        pipe(_final_jobs_formatter, region).\
-        pipe(_enforce_geo_universe, region) \
+    return df \
+        .pipe(_final_jobs_formatter, region) \
+        .pipe(_enforce_geo_universe, region) \
         [[
             'fips', 'name', 'geo_level', 'year', 'demographic-type', 
             'demographic-code', 'demographic', 'contribution', 'compensation',
@@ -262,9 +295,9 @@ def final_data_transform(df, region):
 
 
 def _region_all_pipeline(region):
-    return _raw_data_merge(region).\
-            pipe(_indicators_create).\
-            pipe(final_data_transform, region)
+    return _raw_data_merge(region) \
+            .pipe(_indicators_create) \
+            .pipe(final_data_transform, region)
 
 
 def _download_csv_save(df, aws_filepath):
@@ -276,14 +309,14 @@ def _download_csv_save(df, aws_filepath):
 
 
 def _temp_formatter(df, index_cols):
-    return df.\
-        append(
-            df.query('demographic == "Ages 0 to 1"').\
-            assign(demographic='')
-        ).\
-        sort_values(index_cols).reset_index(drop=True).\
-        drop(columns=['name', 'geo_level', 'demographic-code']).\
-        rename(columns={'fips':'region'})
+    return df \
+        .append(
+            df.query('demographic == "Ages 0 to 1"') \
+            .assign(demographic='')
+        ) \
+        .sort_values(index_cols).reset_index(drop=True) \
+        .drop(columns=['name', 'geo_level', 'demographic-code']) \
+        .rename(columns={'fips':'region'})
 
 
 def _download_to_alley_formatter(df, outcome):
@@ -291,19 +324,23 @@ def _download_to_alley_formatter(df, outcome):
         'fips', 'name', 'geo_level', 'demographic-type', 'demographic-code', 
         'demographic'
     ]
-    return df.\
-        pivot(index=index_cols, columns='year', values=outcome).\
-        reset_index().\
-        pipe(_temp_formatter, index_cols)
+    return df \
+        .pivot(index=index_cols, columns='year', values=outcome) \
+        .reset_index() \
+        .pipe(_temp_formatter, index_cols)
 
 
 def _website_csvs_save(df, aws_filepath):
     for indicator in ['contribution', 'compensation', 'constancy', 'creation']:
         df_out = df.pipe(_download_to_alley_formatter, indicator)
 
-        df_out.to_csv(c.filenamer(f'data/eji_website_{indicator}.csv'), index=False)
+        df_out.to_csv(
+            c.filenamer(f'data/eji_website_{indicator}.csv'), index=False
+        )
         if aws_filepath:
-            df_out.to_csv(f'{aws_filepath}/eji_website_{indicator}.csv', index=False)
+            df_out.to_csv(
+                f'{aws_filepath}/eji_website_{indicator}.csv', index=False
+            )
 
 
 def _raw_data_remove(remove_data=True):
@@ -311,22 +348,26 @@ def _raw_data_remove(remove_data=True):
         shutil.rmtree(c.filenamer('data/temp'))  # remove unwanted files
 
 
-def eji_data_create_all(raw_data_fetch, raw_data_remove, qwi_n_threads, aws_filepath=None):
+def eji_data_create_all(
+    raw_data_fetch, raw_data_remove, qwi_n_threads, aws_filepath=None
+):
     """
     Create and save EJI data. This is the main function of eji_command.py. 
 
-    Fetch raw QWI, PEP, and MSA-crosswalk data, transform it, and save it to two csv's: One for 
-    user download, and one formatted for upload to the Kauffman site.
+    Fetch raw QWI, PEP, and MSA-crosswalk data, transform it, and save it to two
+    csv's: One for user download, and one formatted for upload to the Kauffman 
+    site.
     """
     _raw_data_fetch(raw_data_fetch, qwi_n_threads)
 
     pd.concat(
         [
-            _region_all_pipeline(region) for region in ['us', 'state', 'msa', 'county']
+            _region_all_pipeline(region) 
+            for region in ['us', 'state', 'msa', 'county']
         ]
-    ).\
-        pipe(_download_csv_save, aws_filepath). \
-        pipe(_website_csvs_save, aws_filepath)
+    ) \
+        .pipe(_download_csv_save, aws_filepath) \
+        .pipe(_website_csvs_save, aws_filepath)
 
     _raw_data_remove(raw_data_remove)
 
--- tools/eji_raw_data_fetch.py
+++ tools/eji_raw_data_fetch.py
@@ -8,15 +8,16 @@ from kauffman.tools import consistent_releases
 
 def _pep_county_adjustments(df, region):
     if region == 'county':
-        return df. \
-            assign(
+        return df \
+            .assign(
                 fips=lambda x: x.fips.replace(
                     {'02270':'02158', '46113':'46102', '51515':'51019'}
                 ),
-                region=lambda x: x.region.replace('Bedford city', 'Bedford County')
-            ). \
-            groupby(['time', 'fips', 'region', 'geo_level']).sum(). \
-            reset_index()
+                region=lambda x: x.region \
+                    .replace('Bedford city', 'Bedford County')
+            ) \
+            .groupby(['time', 'fips', 'region', 'geo_level']).sum() \
+            .reset_index()
     else:
         return df
 
@@ -24,39 +25,53 @@ def _pep_county_adjustments(df, region):
 def raw_data_update(qwi_n_threads):
     # if not consistent_releases(n_threads=qwi_n_threads):
     #     raise Exception(
-    #         'There are multiple releases currently in use for the QWI data. ' \
+    #         'There are multiple releases currently in use for the QWI data. '
     #         'Please either wait for the Census to finish state updates on '
-    #         'the latest release, or assemble the data manually using the ' \
+    #         'the latest release, or assemble the data manually using the '
     #         'files at this link: https://lehd.ces.census.gov/data/qwi/'
     #     )
     
-    joblib.dump(str(pd.to_datetime('today')), c.filenamer('data/raw_data/raw_data_fetch_time.pkl'))
+    joblib.dump(
+        str(pd.to_datetime('today')), 
+        c.filenamer('data/raw_data/raw_data_fetch_time.pkl')
+    )
 
     qwi(['EarnBeg'], geo_level='us', private=True, annualize=True) \
-        [['time', 'EarnBeg']]. \
-        rename(columns={'EarnBeg': 'EarnBeg_us'}). \
-        apply(pd.to_numeric). \
-        to_csv(c.filenamer('data/raw_data/earnbeg_us.csv'), index=False)
+        [['time', 'EarnBeg']] \
+        .rename(columns={'EarnBeg': 'EarnBeg_us'}) \
+        .apply(pd.to_numeric) \
+        .to_csv(c.filenamer('data/raw_data/earnbeg_us.csv'), index=False)
 
     for region in ['us', 'state', 'msa', 'county']:
-        qwi(['Emp', 'EmpEnd', 'EarnBeg', 'EmpS', 'EmpTotal', 'FrmJbC'], geo_level=region, private=True, firm_char=['firmage'], annualize=True, n_threads=qwi_n_threads).\
-            to_csv(c.filenamer(f'data/raw_data/qwi_{region}.csv'), index=False)
+        qwi(
+                ['Emp', 'EmpEnd', 'EarnBeg', 'EmpS', 'EmpTotal', 'FrmJbC'], 
+                geo_level=region, 
+                private=True, 
+                firm_char=['firmage'], 
+                annualize=True, 
+                n_threads=qwi_n_threads
+            ) \
+            .to_csv(c.filenamer(f'data/raw_data/qwi_{region}.csv'), index=False)
 
-        pep(region).\
-            query('2001 <= time <= 2020'). \
-            pipe(_pep_county_adjustments, region). \
-            to_csv(c.filenamer(f'data/raw_data/pep_{region}.csv'), index=False)
+        pep(region) \
+            .query('2001 <= time <= 2020') \
+            .pipe(_pep_county_adjustments, region) \
+            .to_csv(c.filenamer(f'data/raw_data/pep_{region}.csv'), index=False)
 
 
 def s3_update():
     files_lst = [
-        'raw_data_fetch_time.pkl', 'qwi_us.csv', 'qwi_state.csv', 'qwi_msa.csv', 'qwi_county.csv',
-        'pep_us.csv', 'pep_state.csv', 'pep_msa.csv', 'pep_county.csv',
-        'earnbeg_us.csv'
+        'raw_data_fetch_time.pkl', 'qwi_us.csv', 'qwi_state.csv', 'qwi_msa.csv', 
+        'qwi_county.csv', 'pep_us.csv', 'pep_state.csv', 'pep_msa.csv', 
+        'pep_county.csv', 'earnbeg_us.csv'
     ]
 
     for file in files_lst:
-        file_to_s3(c.filenamer(f'data/raw_data/{file}'), 'emkf.data.research', f'indicators/eji/raw_data/{file}')
+        file_to_s3(
+            c.filenamer(f'data/raw_data/{file}'), 
+            'emkf.data.research', 
+            f'indicators/eji/raw_data/{file}'
+        )
 
 
 def main():
